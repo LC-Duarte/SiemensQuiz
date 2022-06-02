42271338b7c3efc54d3d6a124fea45c8ed4fd5f0
Leonardo Chaves Duarte <leonardo.duarte.ext@siemens.com>
2022-06-02 14:47:57 -0300

    remove alternate option for 2

--- 2.cpp
+++ 2.cpp
@@ -1,21 +1,8 @@
 #include <iostream>
 #include <string>
-#include<ctime>
+
 #define MAX 100
 
-void tailFooBaa(int i, int c=0){
-    std::string msg ="";
-    if(i%3 == 0)
-        msg += "Foo";
-    if(i%5 == 0)
-        msg += "Baa";
-    else if (i%3 != 0)
-        msg+=  std::to_string(i);
-    std::cout << msg << std::endl ;
-    if (i<MAX){
-        tailFooBaa(i+1, c+1);
-    }
-}
 
 void forFooBaa(int i0){
     for( int i = i0; i<MAX; i++){
@@ -32,14 +19,9 @@ void forFooBaa(int i0){
 }
 
 int main(){
-    std::time_t t0, t1, t2, t3;
-    std::time(&t0);
+
     forFooBaa(0);
-    std::time(&t1);
-    std::time(&t2);
-    tailFooBaa(0);
-    std::time(&t3);
-    std::cout << std::to_string(t1-t0) << std::endl ;
-    std::cout << std::to_string(t3-t2) << std::endl ;
+  
+    
 }
 
